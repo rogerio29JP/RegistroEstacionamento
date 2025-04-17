d26ff40d1696ec925c57cd646ffea7bf08cda85b
rogerio29JP <rogerio.junior@prof.sc.senac.br>
2025-04-17 10:44:27 -0300

    Implementei a estrutura condicional

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -5,6 +5,12 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		System.out.println("Olá mundo");
+		
+		int numero = 10;
+		
+		if (numero > 5) {
+			System.out.println("Sim o numero é maior que 5");
+		}
 
 	}
 
